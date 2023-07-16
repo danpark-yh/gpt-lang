b4f19dc82936e4ed0290da077fd989a3a52a735d
Daniel Park <danpark.yh@gmail.com>
2023-07-16 23:39:59 +1000

    address new type

--- app/api/gpt/route.ts
+++ app/api/gpt/route.ts
@@ -1,8 +1,24 @@
-import { UserPromptOutputLanguage, UserPromptResultOption } from "@/common/type"
+import { UserPromptLanguage, UserPromptResultOption } from "@/common/type"
 import { GPTRequestBody, GPTResponse } from "@/common/type/api"
 import { NextResponse } from "next/server"
 import { Configuration, OpenAIApi } from "openai"
 
+/**
+ * Please proof read below message by following the instructions.
+
+----
+Instructions:
+
+1. Please start with "Edited message: " for proofread message
+2. Explain why you made the change starting with "Edit explanation: " with bullet point format.
+3. If you don't make any changes, please say "No need to change"
+----
+
+----
+message:
+""" Hi my name is Daniel Park, I want need to talk about my life espeically about my elementary school ages. Please listen to me carefully today!"""
+----
+ */
 interface CustomRequest extends Request {
   json(): Promise<GPTRequestBody>
 }
@@ -18,15 +34,13 @@ export async function POST(
     userPrompt,
     userPromptType,
     userPromptResultOption,
-    userPromptInputLanguage,
-    userPromptOutputLanguage,
+    userPromptExplanationLanguage,
   } = body
 
   console.log({ userPrompt })
   console.log({ userPromptType })
   console.log({ userPromptResultOption })
-  console.log({ userPromptInputLanguage })
-  console.log({ userPromptOutputLanguage })
+  console.log({ userPromptExplanationLanguage })
 
   const openai = new OpenAIApi(configuration)
 
@@ -68,7 +82,7 @@ export async function POST(
   answerExplanation = explanation.trim()
 
   if (
-    userPromptOutputLanguage !== UserPromptOutputLanguage.ENGLISH &&
+    userPromptExplanationLanguage !== UserPromptLanguage.ENGLISH &&
     answerExplanation
   ) {
     console.log("TRANSLATION....")
@@ -77,11 +91,11 @@ export async function POST(
       messages: [
         {
           role: "system",
-          content: `You are a professional translator from English to ${userPromptOutputLanguage}`,
+          content: `You are a professional translator from English to ${userPromptExplanationLanguage}`,
         },
         {
           role: "user",
-          content: `The text is the explanation of what have changed to fix English grammars. Please translate this text to ${userPromptOutputLanguage}. Please keep the phrase, sentence and words wraps with "" in English. ----${answerExplanation}`,
+          content: `The text is the explanation of what have changed to fix English grammars. Please translate this text to ${userPromptExplanationLanguage}. Please keep the phrase, sentence and words wraps with "" in English. ----${answerExplanation}`,
         },
       ],
     })
